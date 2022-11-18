47802585e9971af3a31ea6a1375eb32ca250044f
LAPTOP-JHFREC7V\user <s109321022@mail1.ncnu.edu.tw>
2022-11-18 19:07:32 +0800

    rename & add

--- add.py
+++ add.py
@@ -3,9 +3,10 @@
 #處理stdio輸出編碼，以避免亂碼
 import codecs, sys 
 sys.stdout = codecs.getwriter('utf8')(sys.stdout.buffer)
-import cgi
-import goods as gb
+import cgi #import goods as gb
 
+
+from dbConfig import conn, cur
 #先印出http 表頭
 print("Content-Type: text/html; charset=utf-8\n")
 print("""
@@ -13,7 +14,7 @@ print("""
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>範例1</title>
+<title>完成</title>
 </head>
 <body>
 """)
@@ -23,16 +24,11 @@ form = cgi.FieldStorage()
 name=form.getvalue('name')
 price=form.getvalue('price')
 amount=form.getvalue('amount')
-nick=form.getvalue('n')
-"""sql="insert into goods (title,msg,nickname) values (%s,%s,%s);"
-cur.execute(sql,(title,msg,nick))
-conn.commit()"""
-if gb.addMsg(name,price,amount,nick):#(title,msg,nick)
-    print("商品已加入!")
-else:
-    print("add failed!")
-
-print("<br><a href='listMsg.py'>回留言板</a>")
+sql="insert into goods (name,price,amount) values (%s,%s,%s);"
+cur.execute(sql,(name,price,amount))
+conn.commit()
+print("商品已加入!")
+print("<br><a href='main.html'>回商城</a>")
 print("</body></html>")
 
 """
